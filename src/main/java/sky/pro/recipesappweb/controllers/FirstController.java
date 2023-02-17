dbb8dc4b87f4b2201da54b72dc526b2f95db3cb7
ruslankorotkov <richard2008@rus>
2023-02-17 12:41:52 +0300

    Дебаг RecipesAppWeb - приложение для сайта рецептов. Домашка от 17.02.2023

--- src/main/java/sky/pro/recipesappweb/controllers/FirstController.java
+++ src/main/java/sky/pro/recipesappweb/controllers/FirstController.java
@@ -1,7 +1,6 @@
 package sky.pro.recipesappweb.controllers;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
